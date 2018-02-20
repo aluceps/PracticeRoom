ea6ad51304d30bb55c15551d7eee0d6556527fc4
aluceps <aluceps@gmail.com>
2018-02-20 12:53:51 +0900

    エンティティとDAOなどRoomの使用に必要なクラスを実装

---
+++ app/src/main/java/me/aluceps/practiceroom/User.kt
@@ -0,0 +1,15 @@
+package me.aluceps.practiceroom
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity
+data class User(
+        @PrimaryKey
+        val uid: Int,
+        @ColumnInfo(name = "first_name")
+        val firstName: String,
+        @ColumnInfo(name = "last_name")
+        val lastName: String
+)
---
+++ app/src/main/java/me/aluceps/practiceroom/UserDao.kt
@@ -0,0 +1,24 @@
+package me.aluceps.practiceroom
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Delete
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAll(): List<User>
+
+    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+    fun loadAllByIds(userIds: List<Int>): List<User>
+
+    @Query("SELECT * FROM user WHERE first_name LIKE :first AND last_name LIKE :last LIMIT 1")
+    fun findByName(first: String, last: String): User
+
+    @Insert
+    fun insertAll(vararg users: User)
+
+    @Delete
+    fun delete(user: User)
+}
