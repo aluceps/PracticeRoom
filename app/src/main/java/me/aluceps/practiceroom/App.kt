335b7920e148b68770040ea54f7eaff52d245bde
aluceps <aluceps@gmail.com>
2018-02-20 13:08:58 +0900

    アプリケーションクラスとアクティビティの初期化処理を実装

--- app/src/main/java/me/aluceps/practiceroom/App.kt
+++ app/src/main/java/me/aluceps/practiceroom/App.kt
@@ -1,9 +1,19 @@
 package me.aluceps.practiceroom
 
 import android.app.Application
+import android.arch.persistence.room.Room
 
 class App : Application() {
+
+    lateinit var database: AppDatabase
+
     override fun onCreate() {
         super.onCreate()
+        initializeDatabase()
+    }
+
+    private fun initializeDatabase() {
+        database = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "practice-room")
+                .build()
     }
 }
--- app/src/main/java/me/aluceps/practiceroom/MainActivity.kt
+++ app/src/main/java/me/aluceps/practiceroom/MainActivity.kt
@@ -6,9 +6,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var dao: UserDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeDatabase()
+        initializeView()
+    }
+
+    private fun initializeDatabase() {
+        dao = App().database.userDao()
+    }
+
+    private fun initializeView() {
         view.text = "Hello World..."
+
+        add.setOnClickListener { }
+        show.setOnClickListener { }
+        clear.setOnClickListener { }
     }
 }
