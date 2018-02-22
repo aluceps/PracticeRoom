35b9887dd7cb3a826f764456a9bce97f1a4da543
aluceps <aluceps@gmail.com>
2018-02-22 19:57:10 +0900

    警告を消す

--- app/src/main/java/me/aluceps/practiceroom/data/db/AppDatabase.kt
+++ app/src/main/java/me/aluceps/practiceroom/data/db/AppDatabase.kt
@@ -5,7 +5,7 @@ import android.arch.persistence.room.RoomDatabase
 import me.aluceps.practiceroom.data.db.dao.UserDao
 import me.aluceps.practiceroom.data.db.entity.User
 
-@Database(entities = [User::class], version = 2)
+@Database(entities = [User::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 }
