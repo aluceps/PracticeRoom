e013d6b7fd4c80a1be96c4204b5d15d0f9d68191
aluceps <aluceps@gmail.com>
2018-02-22 11:35:35 +0900

    拡張関数を使用して修正

--- app/src/main/java/me/aluceps/practiceroom/MainActivity.kt
+++ app/src/main/java/me/aluceps/practiceroom/MainActivity.kt
@@ -3,66 +3,51 @@ package me.aluceps.practiceroom
 import android.arch.persistence.room.Room
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import me.aluceps.practiceroom.data.db.AppDatabase
-import me.aluceps.practiceroom.data.db.dao.UserDao
 import me.aluceps.practiceroom.data.db.entity.User
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var db: AppDatabase
+    private val db by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "practice-room.db")
+                .fallbackToDestructiveMigration()
+                .build()
+    }
 
-    private lateinit var user: UserDao
+    private val user by lazy {
+        db.userDao()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initializeDatabase()
         initializeView()
     }
 
     override fun onDestroy() {
-        destroyDatabase()
-        super.onDestroy()
-    }
-
-    private fun initializeDatabase() {
-        db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "practice-room")
-                .fallbackToDestructiveMigration()
-                .build()
-
-        user = db.userDao()
-    }
-
-    private fun destroyDatabase() {
         db.close()
+        super.onDestroy()
     }
 
     private fun initializeView() {
         view.text = "Hello World..."
-
-        add.setOnClickListener {
-            insert()
-        }
-        show.setOnClickListener {
-            selectAll()
-        }
-        clear.setOnClickListener {
-            clear()
-        }
+        add.setOnClickListener { insert() }
+        show.setOnClickListener { selectAll() }
+        clear.setOnClickListener { clear() }
     }
 
     private fun insert() {
         var text = ""
         thread {
-            val position = user.size() + 1
-            val item = User(position, "taro", "ngsw", 24)
             db.using(Runnable {
+                val item = User(user.size(), "taro", "ngsw", 24)
                 user.insert(item)
                 text += "${item.uid}: ${item.firstName} ${item.lastName}\n"
             })
-            setText(text)
+            view.text(text)
         }
     }
 
@@ -70,7 +55,7 @@ class MainActivity : AppCompatActivity() {
         var text = ""
         thread {
             user.getAll().forEach { text += "${it.uid}: ${it.firstName} ${it.lastName}\n" }
-            setText(text)
+            view.text(text)
         }
     }
 
@@ -79,16 +64,14 @@ class MainActivity : AppCompatActivity() {
             db.using(Runnable {
                 user.getAll().forEach { user.delete(it) }
             })
-            setText("")
+            view.text("")
         }
     }
 
-    private fun setText(value: String) {
-        view.apply {
-            post({
-                text = value
-            })
-        }
+    private fun TextView.text(value: String) {
+        post({
+            text = value
+        })
     }
 
     private fun AppDatabase.using(runnable: Runnable) {
